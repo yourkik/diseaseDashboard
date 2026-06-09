2ab222038cc2906f8da0a03632e02a92df72e4db
yourkik <yourkik5@gmail.com>
2026-06-09 14:33:50 +0900

    delete Frontend deploy workflow

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  // Azure Static Web Apps에서는 이미지를 위한 서버 최적화가 지원되지 않으므로 unoptimized 옵션 활성화
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
